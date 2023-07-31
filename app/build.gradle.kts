00e8556f49aa61582ebf39b122d597ea32fb56ce
LuisEduardoGouvea <luis.silva-ext@viavarejo.com.br>
2023-07-31 20:16:35 -0300

    libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,12 +40,37 @@ android {
 
 dependencies {
 
+    val retrofit_version = "2.9.0"
+    val koin_version = "3.1.5"
+    val navigation_version = "2.4.1"
+
+    // AndroidX
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-common:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigation_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigation_version")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    // Koin
+    implementation("io.insert-koin:koin-android:$koin_version")
+    implementation("io.insert-koin:koin-android-compat:$koin_version")
+    // Koin for JUnit 4
+    implementation("io.insert-koin:koin-test-junit4:$koin_version")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-moshi:$retrofit_version")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+
+    // Material
+    implementation("com.google.android.material:material:1.8.0")
+
+    // Tests
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
