2f6826d50e4402d2970460a9191cf516a203f596
Luis Eduardo <luiseduardo.gouvea@gmail.com>
2025-02-07 12:09:56 -0300

    removed koin code

--- app/src/main/java/com/example/nowinjetpack/CustomApp.kt
+++ app/src/main/java/com/example/nowinjetpack/CustomApp.kt
@@ -2,7 +2,6 @@ package com.example.nowinjetpack
 
 import android.app.Application
 import androidx.lifecycle.LifecycleObserver
-import com.example.nowinjetpack.core.common.ModuleInitializer
 import dagger.hilt.android.HiltAndroidApp
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -12,17 +11,5 @@ open class CustomApp :
     Application(),
     LifecycleObserver {
 
-    override fun onCreate() {
-        super.onCreate()
-        //setupKoin()
-    }
 
-    private fun setupKoin() {
-        startKoin {
-            modules(
-                ModuleInitializer.modules
-            )
-            androidContext(this@CustomApp)
-        }
-    }
 }
